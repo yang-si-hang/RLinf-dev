37d16b54afd07d747b8bd9808ea137cc9bdb8cba
yang-sihang <yang_si_hang@126.com>
2026-09-23 15:25:01 +0800

    update server RLT config

---
+++ docker/env_variable.sh
@@ -0,0 +1,7 @@
+export HF_LEROBOT_HOME=/root/autodl-tmp/workspace/RLinf-dev/data/lerobot_data
+export WANDB_API_KEY=${WANDB_API_KEY:?WANDB_API_KEY is required} 
+export WANDB_MODE=online
+export PROJECT_ROOT=/root/autodl-tmp/workspace/RLinf-dev
+
+export http_proxy=http://127.0.0.1:7897
+export https_proxy=http://127.0.0.1:7897
